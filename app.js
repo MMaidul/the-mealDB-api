e3688deacb88b2209777a6b41a8bd453d5774dbf
MMaidul <maidulmuqtadir360@gmail.com>
2022-02-27 12:53:54 +0600

    single ui displyed by mealId

--- app.js
+++ app.js
@@ -36,13 +36,33 @@ const displaySearchMeals = meals => {
 
 
 const singleMealInfo = mealId => {
-    console.log(mealId);
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i= ${mealId}`)
         .then(res => res.json())
-        .then(data => singleDisplayMeal(data.meals))
+        .then(data => singleDisplayMeal(data.meals[0]))
 }
 
 
 const singleDisplayMeal = meals => {
     console.log(meals);
+    const singleSearchResult = document.getElementById('single-result');
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card mb-3">
+    <div class="row g-0">
+        <div class="col-md-4">
+            <img src="..." class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.</p>
+                <a href="" class="btn btn-success">Learn form tutorial</a>
+            </div>
+        </div>
+    </div>
+   </div>
+    `;
+
+    singleSearchResult.appendChild(div);
 }
